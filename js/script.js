bbf36e96e53a7c7ae974cc088bfc31b55d7793ec
Denny29 <dennyrfernandez2004@gmail.com>
2020-02-28 01:41:31 +0000

    Just about finished up HTML and CSS. Working on jquery now

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-$(".communism").click(function(){
-    $(".service").show();
-});
+// $(".communism").hover(function(){
+    // $(".service").show();
+    // $(".service").css("background-image: url('https://ak2.picdn.net/shutterstock/videos/507802/thumb/1.jpg');"); Background img easter egg
+// });
 $(".addButton").click(function(){
-    $(".add").show();
+    $(".addSongs").show();
+});
+
+let song = $(".song").val();
+let artist = $(".artist").val();
+let length = $(".length").val();
+let albumCover = $(".albumCover").val();
+let videoLink = $(".videoLink").val();
+
+$(".confirm").click(function(){
+    $(".songs").append('<div>'+song+'</div>');
 });
